fc70a3ebef750a6f89ac55ac14ccfe4d0785002a
Robin <robinhuizhang@gmail.com>
2014-05-25 15:22:17 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 # Source of data for the project:
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-
 # This R script does the following:
 # 1. Merges the training and the test sets to create one data set.
 
